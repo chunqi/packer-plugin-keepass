a3913fdaafede4c1cc910b8fe7aa1d4684082c67
Chunqi Zhu <chunqi@users.noreply.github.com>
2022-01-11 23:48:46 +0800

    Added implementation of entry root key to upload all attachments for entry

--- provisioner/attachment/provisioner.go
+++ provisioner/attachment/provisioner.go
@@ -5,7 +5,6 @@ package attachment
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 
@@ -77,40 +76,52 @@ func (p *Provisioner) Provision(_ context.Context, ui packer.Ui, communicator pa
 	if err != nil {
 		return err
 	}
-	// generate map of file attachmentsMap
-	attachmentsMap := map[string]*gokeepasslib.Binary{}
+	// generate map of file attachments
+	attachmentsMap := map[string]gokeepasslib.BinaryReference{}
+	entryMap := map[string]gokeepasslib.Entry{}
 	entryCallback := func(entryPath string, entry gokeepasslib.Entry, depth int) {
+		entryMap[entryPath] = entry
 		for _, attachment := range entry.Binaries {
-			attachmentBinary := attachment.Find(db)
 			entryAttachmentPath := fmt.Sprintf("%s-%s", entryPath, attachment.Name)
-			if attachmentBinary != nil {
-				attachmentsMap[entryAttachmentPath] = attachmentBinary
-				log.Println(fmt.Sprintf("(file) %s", entryAttachmentPath))
-			} else {
-				log.Println(fmt.Sprintf("[WARNING] Could not find attachment binary for file: %s", entryAttachmentPath))
-			}
+			attachmentsMap[entryAttachmentPath] = attachment
 		}
 	}
 	common.WalkDatabase(db, nil, entryCallback)
-	// check that the requested file is in the map
 	if _, keyExists := attachmentsMap[attachmentPath]; keyExists {
-		p.ProvisionUpload(ui, communicator, attachmentsMap[attachmentPath])
-		return nil
+		// if the specified attachmentPath is in the attachmentsMap, upload the attachment
+		attachment := attachmentsMap[attachmentPath]
+		// if the destination is a directory, append with the attachment file name
+		if strings.HasSuffix(p.config.Destination, "/") {
+			p.config.Destination = p.config.Destination + attachment.Name
+		}
+		return p.UploadAttachment(ui, communicator, db, attachment)
+	} else if _, keyExists := entryMap[attachmentPath]; keyExists {
+		// if the specified attachmentPath is an entry root path, upload all attachments within
+		entry := entryMap[attachmentPath]
+		attachmentsCount := len(entry.Binaries)
+		ui.Say(fmt.Sprintf("Uploading %d attachments from entry %s", attachmentsCount, attachmentPath))
+		return p.UploadAttachments(ui, communicator, db, entry.Binaries)
 	} else {
 		return fmt.Errorf("File attachment \"%s\" does not exist.", attachmentPath)
 	}
 }
 
-// Uploads the attachment file to the destination path using a temp file
-func (p *Provisioner) ProvisionUpload(ui packer.Ui, communicator packer.Communicator, attachment *gokeepasslib.Binary) error {
-	ui.Say(fmt.Sprintf("Uploading %s => %s", p.config.AttachmentPath, p.config.Destination))
-	// create temp file to hold attachment contents
-	attachmentTempFile, err := os.CreateTemp("", "keepass-attachment")
+// Upload a single file attachment to the destination path using a temp file
+func (p *Provisioner) UploadAttachment(ui packer.Ui, communicator packer.Communicator, db *gokeepasslib.Database, attachment gokeepasslib.BinaryReference) error {
+	// retrieve the attachment object
+	attachmentBinary := attachment.Find(db)
+	if attachmentBinary == nil {
+		return fmt.Errorf("Could not find attachment binary for file: %s", attachment.Name)
+	}
+	ui.Say(fmt.Sprintf("Uploading %s => %s", attachment.Name, p.config.Destination))
+	// create temp file for the attachment contents
+	attachmentTempFile, err := os.CreateTemp(os.TempDir(), "keepass-attachment")
 	if err != nil {
 		return err
 	}
 	defer attachmentTempFile.Close()
-	attachmentBytes, err := attachment.GetContentBytes()
+	defer os.Remove(attachmentTempFile.Name())
+	attachmentBytes, err := attachmentBinary.GetContentBytes()
 	if err != nil {
 		return err
 	} else {
@@ -121,7 +132,7 @@ func (p *Provisioner) ProvisionUpload(ui packer.Ui, communicator packer.Communic
 		if err != nil {
 			return err
 		}
-		attachmentTempFileReader := ui.TrackProgress("test", 0, attachmentTempFileInfo.Size(), attachmentTempFile)
+		attachmentTempFileReader := ui.TrackProgress(attachment.Name, 0, attachmentTempFileInfo.Size(), attachmentTempFile)
 		defer attachmentTempFileReader.Close()
 		if err = communicator.Upload(p.config.Destination, attachmentTempFileReader, &attachmentTempFileInfo); err != nil {
 			if strings.Contains(err.Error(), "Error restoring file") {
@@ -132,6 +143,46 @@ func (p *Provisioner) ProvisionUpload(ui packer.Ui, communicator packer.Communic
 		}
 		return nil
 	}
+}
+
+// Upload entry file attachment(s) to the destination path using a temp dir
+func (p *Provisioner) UploadAttachments(ui packer.Ui, communicator packer.Communicator, db *gokeepasslib.Database, attachments []gokeepasslib.BinaryReference) error {
+	// create temp dir to hold file attachments
+	attachmentsTempDir, err := os.MkdirTemp(os.TempDir(), "keepass-attachments")
+	if err != nil {
+		return err
+	}
+	// write each file attachment as a temp file
+	for _, attachment := range attachments {
+		// retrieve the attachment object
+		attachmentBinary := attachment.Find(db)
+		if attachmentBinary == nil {
+			ui.Error(fmt.Sprintf("[WARNING] Could not find attachment binary for file: %s, skipping", attachment.Name))
+			continue
+		}
+		// create temp file to hold attachment contents
+		attachmentFile, err := os.Create(attachmentsTempDir + "/" + attachment.Name)
+		if err != nil {
+			return err
+		}
+		// write attachment contents to temp file
+		attachmentBytes, err := attachmentBinary.GetContentBytes()
+		if err != nil {
+			return err
+		} else {
+			_, err := attachmentFile.Write(attachmentBytes)
+			if err != nil {
+				return err
+			}
+		}
+		attachmentFile.Close()
+		ui.Say(fmt.Sprintf("File: %s", attachment.Name))
+	}
+	// upload dir
+	err = communicator.UploadDir(p.config.Destination, attachmentsTempDir+"/", nil)
+	// cleanup temp dir and contents
+	os.RemoveAll(attachmentsTempDir)
+	return err
 }
 
 // Check that attachment_path and destination config are provided
